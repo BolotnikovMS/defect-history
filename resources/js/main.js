9f210a0ba8e1ce167d955aea32638a9ebacde704
BolotnikovMS <bolotnikovms@yandex.ru>
2022-11-07 21:03:35 +0500

    Добавлена функция для открытия/скрытия контента

---
+++ resources/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Open dialog
+
+  const openDialog = (selectorDialog, selectorBtnTrigger) => {
+    const dialog = document.querySelector(selectorDialog)
+    const btnTrigger = document.querySelector(selectorBtnTrigger)
+
+    btnTrigger.addEventListener('click', () => {
+      dialog.classList.toggle('active')
+    })
+  }
+
+  openDialog('.dialog', '.defect__btn-open')
+
+  // Alerts
+  const closeBtn = document.querySelector('.alert__btn-close')
+  const message = document.querySelector('.alert')
+
+  closeBtn.addEventListener('click', () => {
+    message.classList.toggle('active')
+  })
+})
