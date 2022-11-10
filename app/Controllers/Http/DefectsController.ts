0b76d67bbfcd4a2dbd1158c151567f2292e8ad44
BolotnikovMS <bolotnikovms@yandex.ru>
2022-11-10 21:05:36 +0500

    Добавлены методы для сохранения и удаления записи

--- app/Controllers/Http/DefectsController.ts
+++ app/Controllers/Http/DefectsController.ts
@@ -1,19 +1,50 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Defect from 'App/Models/Defect'
+import Substation from 'App/Models/Substation'
+import DefectValidator from 'App/Validators/DefectValidator'
+import DefectType from '../../Models/DefectType'
 
 export default class DefectsController {
-  public async index({ request, view }: HttpContextContract) {
-    const defects = await Defect.query()
+  public async index({ view }: HttpContextContract) {
+    const defects = await Defect.query().preload('defect_type').preload('substation')
+    const typeDefects = await DefectType.query()
+    const substations = await Substation.query()
 
     return view.render('pages/defect/index', {
       title: 'Все дефекты',
       defects,
+      typeDefects,
+      substations,
     })
   }
 
   public async create({}: HttpContextContract) {}
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, response, session }: HttpContextContract) {
+    const validateDefectData = await request.validate(DefectValidator)
+
+    if (validateDefectData) {
+      const defect = {
+        id_substation: +validateDefectData.substation,
+        id_type_defect: +validateDefectData.defect_type,
+        id_user: 1,
+        ...validateDefectData,
+        term_elimination: validateDefectData.term_elimination
+          .replace(new RegExp('-', 'g'), '.')
+          .split('.')
+          .reverse()
+          .join('.'),
+      }
+
+      await Defect.create(defect)
+
+      session.flash('successMessage', `Дефект успешно добавлен!`)
+      response.redirect().back()
+    } else {
+      session.flash('dangerMessage', 'Что-то пошло не так!')
+      response.redirect().back()
+    }
+  }
 
   public async show({}: HttpContextContract) {}
 
@@ -21,5 +52,17 @@ export default class DefectsController {
 
   public async update({}: HttpContextContract) {}
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ response, params, session }: HttpContextContract) {
+    const defect = await Defect.find(params.id)
+
+    if (defect) {
+      await defect.delete()
+
+      session.flash('successMessage', `Дефект успешно удален!`)
+      response.redirect().back()
+    } else {
+      session.flash('dangerMessage', 'Что-то пошло не так!')
+      response.redirect().back()
+    }
+  }
 }
