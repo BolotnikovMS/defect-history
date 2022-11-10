b0a3bbe703f58e0aa1b6ab566133abb4058e8a32
BolotnikovMS <bolotnikovms@yandex.ru>
2022-11-10 21:00:41 +0500

    Подправлен класс

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  openDialog('.dialog', '.defect__btn-open')
+  openDialog('.dialog', '.btn-open')
 
   // Alerts
   const closeBtn = document.querySelector('.alert__btn-close')
