be376894a2009a17996227a56550ce3c87ed2164
BolotnikovMS <bolotnikovms@yandex.ru>
2022-11-14 22:00:39 +0500

    Поправлены отступы

--- database/migrations/1667131744429_intermediate_checks.ts
+++ database/migrations/1667131744429_intermediate_checks.ts
@@ -3,27 +3,19 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'intermediate_checks'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('id_defect', 10)
-        .notNullable()
-        .unsigned()
-        .index()
-        .references('defects.id')
-      table.integer('id_inspector', 10)
-        .notNullable()
-        .unsigned()
-        .index()
-        .references('staff.id')
-      table.string('check_date',15).notNullable()
+      table.integer('id_defect', 10).notNullable().unsigned().index().references('defects.id')
+      table.integer('id_inspector', 10).notNullable().unsigned().index().references('staff.id')
+      table.string('check_date', 15).notNullable()
       table.text('test_result').notNullable()
 
       table.timestamps()
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
