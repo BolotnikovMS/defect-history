795c70c0723cdcd5a9541732c53490b3b4f98580
BolotnikovMS <bolotnikovms@yandex.ru>
2022-11-27 19:18:26 +0500

    Добавлены методы

--- app/Controllers/Http/DefectsController.ts
+++ app/Controllers/Http/DefectsController.ts
@@ -7,18 +7,29 @@ import DefectType from '../../Models/DefectType'
 export default class DefectsController {
   public async index({ view }: HttpContextContract) {
     const defects = await Defect.query().preload('defect_type').preload('substation')
-    const typeDefects = await DefectType.query()
-    const substations = await Substation.query()
 
     return view.render('pages/defect/index', {
       title: 'Все дефекты',
       defects,
+      activeMenuLink: 'defects.all',
+    })
+  }
+
+  public async create({ view }: HttpContextContract) {
+    const typeDefects = await DefectType.all()
+    const substations = await Substation.all()
+
+    return view.render('pages/defect/form', {
+      title: 'Добавление нового дефекта',
+      options: {
+        routePath: {
+          savePath: 'defect.store',
+        },
+      },
       typeDefects,
       substations,
     })
   }
-
-  public async create({}: HttpContextContract) {}
 
   public async store({ request, response, session }: HttpContextContract) {
     const validateDefectData = await request.validate(DefectValidator)
@@ -29,28 +40,83 @@ export default class DefectsController {
         id_type_defect: +validateDefectData.defect_type,
         id_user: 1,
         ...validateDefectData,
-        term_elimination: validateDefectData.term_elimination
-          .replace(new RegExp('-', 'g'), '.')
-          .split('.')
-          .reverse()
-          .join('.'),
       }
 
       await Defect.create(defect)
 
       session.flash('successMessage', `Дефект успешно добавлен!`)
-      response.redirect().back()
+      response.redirect().toRoute('DefectsController.index')
+    } else {
+      session.flash('dangerMessage', 'Что-то пошло не так!')
+      response.redirect().toRoute('DefectsController.index')
+    }
+  }
+
+  public async show({ params, view }: HttpContextContract) {
+    const defect = await Defect.query()
+      .where('id', '=', params.id)
+      .preload('substation')
+      .preload('defect_type')
+      .preload('intermediate_checks')
+
+    const [defectDes] = defect
+    const defectSerialize = defectDes.serialize()
+
+    return view.render('pages/defect/show', {
+      title: 'Подробный просмотр',
+      defectSerialize,
+    })
+  }
+
+  public async edit({ params, response, view, session }: HttpContextContract) {
+    const defect = await Defect.find(params.id)
+
+    if (defect) {
+      const defectSerialize = defect.serialize()
+      const typeDefects = await DefectType.all()
+      const substations = await Substation.all()
+
+      return view.render('pages/defect/form', {
+        title: 'Редактирование',
+        options: {
+          idData: defect.id,
+          routePath: {
+            savePath: 'defect.update',
+          },
+        },
+        defectSerialize,
+        typeDefects,
+        substations,
+      })
     } else {
       session.flash('dangerMessage', 'Что-то пошло не так!')
       response.redirect().back()
     }
   }
 
-  public async show({}: HttpContextContract) {}
+  public async checkupCreate({}: HttpContextContract) {}
 
-  public async edit({}: HttpContextContract) {}
+  public async update({ params, request, response, session }: HttpContextContract) {
+    const defect = await Defect.find(params.id)
 
-  public async update({}: HttpContextContract) {}
+    if (defect) {
+      const validateDefectData = await request.validate(DefectValidator)
+
+      defect.id_type_defect = +validateDefectData.defect_type
+      defect.id_substation = +validateDefectData.substation
+      defect.accession = validateDefectData.accession
+      defect.description_defect = validateDefectData.description_defect
+      defect.term_elimination = validateDefectData.term_elimination
+
+      await defect.save()
+
+      session.flash('successMessage', `Данные дефекта успешно обновлены.`)
+      response.redirect().toRoute('DefectsController.index')
+    } else {
+      session.flash('dangerMessage', 'Что-то пошло не так!')
+      response.redirect().back()
+    }
+  }
 
   public async destroy({ response, params, session }: HttpContextContract) {
     const defect = await Defect.find(params.id)
