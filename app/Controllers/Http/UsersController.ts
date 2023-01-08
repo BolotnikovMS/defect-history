0259b265942577f3c1fe18ea0f70c9193f7d0420
BolotnikovMS <bolotnikovms@yandex.ru>
2023-01-08 20:28:34 +0500

    Добавлены методы

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,6 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import Roles from '../../Enums/Roles'
+import Role from 'App/Models/Role'
+import { schema, rules } from '@ioc:Adonis/Core/Validator';
 
 export default class UsersController {
   public async index({ request, response, view, session, bouncer }: HttpContextContract) {
@@ -26,9 +28,66 @@ export default class UsersController {
 
   public async show({}: HttpContextContract) {}
 
-  public async edit({}: HttpContextContract) {}
+  public async edit({ params, response, view, session, bouncer }: HttpContextContract) {
+    if (await bouncer.denies('editUser')) {
+      session.flash('dangerMessage', 'У вас нету прав на редактирование!')
 
-  public async update({}: HttpContextContract) {}
+      return response.redirect().toPath('/')
+    }
+
+    const user = await User.find(params.id)
+    const roles = await Role.all()
+    const rolesSerialize = roles.map((role) => {
+      return role.serialize({
+        fields: ['id', 'name'],
+      })
+    })
+
+    if (user) {
+      return view.render('pages/user/edit', {
+        title: 'Редактирование',
+        user,
+        rolesSerialize,
+      })
+    } else {
+      session.flash('dangerMessage', 'Что-то пошло не так!')
+      response.redirect().back()
+    }
+  }
+
+  public async update({ request, response, params, session, bouncer }: HttpContextContract) {
+    if (await bouncer.denies('editUser')) {
+      session.flash('dangerMessage', 'У вас нету прав на редактирование!')
+
+      return response.redirect().toPath('/')
+    }
+
+    const user = await User.find(params.id)
+    const _schema = schema.create({
+      username: schema.string([rules.minLength(2), rules.trim(), rules.escape()]),
+      surname: schema.string([rules.minLength(2), rules.trim(), rules.escape()]),
+      name: schema.string([rules.minLength(2), rules.trim(), rules.escape()]),
+      patronymic: schema.string([rules.minLength(2), rules.trim(), rules.escape()]),
+      position: schema.string([rules.minLength(2), rules.trim(), rules.escape()]),
+      role: schema.number(),
+      blocked: schema.string.optional(),
+    })
+
+    if (user) {
+      const validateData = await request.validate({ schema: _schema })
+
+      await user
+        .merge({
+          ...validateData,
+          blocked: validateData.blocked ? validateData.blocked : 'false',
+          id_role: validateData.role,
+        })
+        .save()
+
+      session.flash('successMessage', `Данные пользователя "${user.fullName}" успешно обновлены.`)
+      response.redirect().toRoute('users.index')
+    }
+  }
 
   public async destroy({ response, params, session, bouncer }: HttpContextContract) {
     const user = await User.find(params.id)
