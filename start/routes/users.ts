d628c318df04c6572b5da17cad8b3fb9012ac7f3
BolotnikovMS <bolotnikovms@yandex.ru>
2023-01-08 20:26:28 +0500

    Добавлены маршруты

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -2,6 +2,8 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', 'UsersController.index').as('index').middleware(['auth'])
+  Route.get('/edit/:id', 'UsersController.edit').as('edit').middleware(['auth'])
+  Route.post('/edit/:id', 'UsersController.update').as('update').middleware(['auth'])
   Route.get('/delete/:id', 'UsersController.destroy').as('destroy').middleware(['auth'])
 })
   .prefix('users')
