766259f954eef64b5a21ec5df75d59ef7f98c189
BolotnikovMS <bolotnikovms@yandex.ru>
2023-01-22 22:29:37 +0500

    Добавлен маршрут show

--- start/routes/substations.ts
+++ start/routes/substations.ts
@@ -4,6 +4,7 @@ Route.group(() => {
   Route.get('/', 'SubstationsController.index').as('index').middleware(['auth'])
   Route.get('/new', 'SubstationsController.create').as('create').middleware(['auth'])
   Route.post('/new', 'SubstationsController.store').as('store').middleware(['auth'])
+  Route.get('/show/:id', 'SubstationsController.show').as('show')
   Route.get('/edit/:id', 'SubstationsController.edit').as('edit').middleware(['auth'])
   Route.post('/edit/:id', 'SubstationsController.update').as('update').middleware(['auth'])
   Route.get('/delete/:id', 'SubstationsController.destroy').as('destroy').middleware(['auth'])
