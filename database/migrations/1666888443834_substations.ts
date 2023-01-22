7328e397a7f034d7005cfb916a254cfed23ed672
BolotnikovMS <bolotnikovms@yandex.ru>
2023-01-22 22:24:40 +0500

    Убрано поле voltage_class

--- database/migrations/1666888443834_substations.ts
+++ database/migrations/1666888443834_substations.ts
@@ -7,9 +7,8 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('id_user', 10).notNullable()
-      table.string('name', 255).notNullable()
-      table.string('importance').checkIn(['true', 'false']).defaultTo('false')
-      table.string('voltage_class', 50)
+      table.text('name').notNullable()
+      table.text('importance').checkIn(['true', 'false']).defaultTo('false')
 
       table.timestamps()
     })
