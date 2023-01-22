7a9407cb13385ef1511b0e4572558c8e700cc721
BolotnikovMS <bolotnikovms@yandex.ru>
2023-01-22 22:26:33 +0500

    Добавлено правило для проверки department

--- app/Validators/AuthValidator.ts
+++ app/Validators/AuthValidator.ts
@@ -4,25 +4,6 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class AuthValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
   public schema = schema.create({
     username: schema.string([
       rules.minLength(2),
@@ -34,6 +15,7 @@ export default class AuthValidator {
     name: schema.string([rules.minLength(2), rules.trim(), rules.escape()]),
     patronymic: schema.string([rules.minLength(2), rules.trim(), rules.escape()]),
     position: schema.string([rules.minLength(2), rules.trim(), rules.escape()]),
+    department: schema.number(),
     role: schema.number(),
     email: schema.string([
       rules.email(),
