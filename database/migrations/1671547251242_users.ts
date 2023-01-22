9c3079cf47b7f7859ae1ebdd9b906448086a0622
BolotnikovMS <bolotnikovms@yandex.ru>
2023-01-22 22:24:05 +0500

    Добавлены поля

--- database/migrations/1671547251242_users.ts
+++ database/migrations/1671547251242_users.ts
@@ -8,12 +8,20 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.integer('id_user_create').notNullable()
       table.string('blocked').notNullable().defaultTo('false')
       table.string('username', 50).notNullable().unique()
       table.string('surname', 30).notNullable()
       table.string('name', 30).notNullable()
       table.string('patronymic', 30).notNullable()
       table.string('position', 50).notNullable()
+      table
+        .integer('id_department', 10)
+        .notNullable()
+        .unsigned()
+        .index()
+        .references('id')
+        .inTable('departments')
       table
         .integer('id_role', 10)
         .notNullable()
