cc1ba149c7239aa2d95ee3909c066c2f9933fe83
BolotnikovMS <bolotnikovms@yandex.ru>
2023-01-23 22:18:30 +0500

    Правила валидации

---
+++ app/Validators/DistrictValidator.ts
@@ -0,0 +1,21 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class DistrictValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    name: schema.string({}, [
+      rules.trim(),
+      rules.escape(),
+      rules.minLength(2),
+      rules.maxLength(110),
+    ]),
+  })
+
+  public messages: CustomMessages = {
+    required: 'Поле является обязательным.',
+    minLength: 'Минимальная длина поля 2 символа.',
+    maxLength: 'Максимальная длина поля 110 символов.',
+  }
+}
