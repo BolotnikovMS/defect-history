9104eae6c0efc1654f3f337b6c4ebd94ac2cb472
BolotnikovMS <bolotnikovms@yandex.ru>
2023-01-25 20:47:41 +0500

    Добавлен путь для просмотра всех объектов

--- start/routes/districts.ts
+++ start/routes/districts.ts
@@ -4,6 +4,7 @@ Route.group(() => {
   Route.get('/', 'DistrictsController.index').as('index').middleware(['auth'])
   Route.get('/new', 'DistrictsController.create').as('create').middleware(['auth'])
   Route.post('/new', 'DistrictsController.store').as('store').middleware(['auth'])
+  Route.get('/show/:id', 'DistrictsController.show').as('show')
   Route.get('/edit/:id', 'DistrictsController.edit').as('edit').middleware(['auth'])
   Route.post('/edit/:id', 'DistrictsController.update').as('update').middleware(['auth'])
   Route.get('/delete/:id', 'DistrictsController.destroy').as('destroy').middleware(['auth'])
