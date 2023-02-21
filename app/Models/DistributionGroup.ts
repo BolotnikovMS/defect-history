ce941a92fb82c081fb1377754b0aa80ce55dd97b
BolotnikovMS <bolotnikovms@yandex.ru>
2023-02-21 21:43:04 +0500

    Модель

---
+++ app/Models/DistributionGroup.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, ManyToMany, column, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class DistributionGroup extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public description: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @manyToMany(() => User, {
+    localKey: 'id',
+    pivotForeignKey: 'id_distribution_group',
+    relatedKey: 'id',
+    pivotRelatedForeignKey: 'id_user',
+    pivotTable: 'distribution_groups_users',
+  })
+  public group_users: ManyToMany<typeof User>
+}
