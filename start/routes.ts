5f45edaf5b19935d569093427b8922732095222a
BolotnikovMS <bolotnikovms@yandex.ru>
2023-02-27 19:55:15 +0500

    Маршруты для групп

--- start/routes.ts
+++ start/routes.ts
@@ -29,6 +29,24 @@ Route.group(() => {
   require('./routes/districts')
   require('./routes/substations')
   require('./routes/staff')
+
+  Route.group(() => {
+    Route.get('/', 'DistributionGroupsController.index').as('index')
+    Route.get('/new', 'DistributionGroupsController.create').as('create')
+    Route.post('/new', 'DistributionGroupsController.store').as('store')
+    Route.get('/show/:id', 'DistributionGroupsController.show').as('show')
+    Route.post('/add-user-in-group/:idGroup', 'DistributionGroupsController.addUserInGroup')
+      .as('add-user')
+    Route.get(
+      '/delete-user-from-group/:idGroup/:idUser',
+      'DistributionGroupsController.removeUserFromGroup'
+    ).as('remove-user')
+    Route.get('/edit/:id', 'DistributionGroupsController.edit').as('edit')
+    Route.post('/edit/:id', 'DistributionGroupsController.update').as('update')
+    Route.get('/delete/:id', 'DistributionGroupsController.destroy').as('destroy')
+  })
+    .prefix('/distribution-groups')
+    .as('distribution')
 }).namespace('App/Controllers/Http')
 
 // Route.group(() => {
