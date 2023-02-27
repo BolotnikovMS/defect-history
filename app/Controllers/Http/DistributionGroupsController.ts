bb3b882a388d4828458ec6a4361223f5308e5b3f
BolotnikovMS <bolotnikovms@yandex.ru>
2023-02-27 19:58:51 +0500

    Добавлена валидация, а также добавление/удаление из группы

--- app/Controllers/Http/DistributionGroupsController.ts
+++ app/Controllers/Http/DistributionGroupsController.ts
@@ -1,10 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import DistributionGroup from 'App/Models/DistributionGroup'
 import DistributionGroupsUser from 'App/Models/DistributionGroupsUser'
+import User from 'App/Models/User'
+import DistributionGroupValidator from 'App/Validators/DistributionGroupValidator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class DistributionGroupsController {
-  public async index({ request, view }: HttpContextContract) {
-    const distributionGroups = await DistributionGroup.query().orderBy('created_at', 'asc')
+  public async index({ view }: HttpContextContract) {
+    const distributionGroups = await DistributionGroup.query()
+      .orderBy('created_at', 'asc')
+      .preload('group_users')
 
     return view.render('pages/distribution/index', {
       title: 'Группы рассылок',
@@ -24,25 +29,24 @@ export default class DistributionGroupsController {
   }
 
   public async store({ request, response }: HttpContextContract) {
-    const group = request.only(['name', 'description'])
-    console.log('group: ', group)
+    const validateDataGroup = await request.validate(DistributionGroupValidator)
 
-    await DistributionGroup.create(group)
+    await DistributionGroup.create(validateDataGroup)
 
     response.redirect().toRoute('distribution.index')
   }
 
   public async show({ response, params, view, session }: HttpContextContract) {
     const group = await DistributionGroup.find(params.id)
+    const users = await User.all()
 
     if (group) {
       await group.load('group_users')
 
-      console.log('group: ', group.serialize())
-
       return view.render('pages/distribution/show', {
         title: `Подробный просмотр группы: '${group.name}'`,
         group,
+        users,
       })
     } else {
       session.flash('dangerMessage', 'Что-то пошло не так!')
@@ -50,9 +54,27 @@ export default class DistributionGroupsController {
     }
   }
 
-  public async addUserInGroup({}: HttpContextContract) {}
+  public async addUserInGroup({ params, request, response, session }: HttpContextContract) {
+    const idGroup = await params.idGroup
+    const _schema = schema.create({
+      user: schema.array([rules.minLength(1), rules.maxLength(15)]).members(schema.number()),
+    })
+    const _messages: CustomMessages = {
+      required: 'Поле является обязательным.',
+    }
 
-  public async saveUserInGroup({}: HttpContextContract) {}
+    const validateDate = await request.validate({ schema: _schema, messages: _messages })
+
+    validateDate.user.forEach(async (user) => {
+      await DistributionGroupsUser.create({
+        id_user: user,
+        id_distribution_group: idGroup,
+      })
+    })
+
+    session.flash('successMessage', `Пользователь успешно добавлен в группу!`)
+    response.redirect().back()
+  }
 
   public async removeUserFromGroup({ response, params, session }: HttpContextContract) {
     const userGroup = await DistributionGroupsUser.query().where(
@@ -94,7 +116,16 @@ export default class DistributionGroupsController {
     }
   }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ params, request, response, session }: HttpContextContract) {
+    const group = await DistributionGroup.find(params.id)
+
+    const validateDataGroup = await request.validate(DistributionGroupValidator)
+
+    await group?.merge(validateDataGroup).save()
+
+    session.flash('successMessage', `Данные группы успешно обновлены.`)
+    response.redirect().toRoute('distribution.index')
+  }
 
   public async destroy({ response, params, session }: HttpContextContract) {
     const group = await DistributionGroup.find(params.id)
