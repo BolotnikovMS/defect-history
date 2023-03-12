19c5d946bd477b708bcac100e5092a5c155f201f
BolotnikovMS <bolotnikovms@yandex.ru>
2023-03-12 20:41:01 +0500

    Добавлены маршруты

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -1,10 +1,14 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('/', 'UsersController.index').as('index').middleware(['auth'])
-  Route.get('/edit/:id', 'UsersController.edit').as('edit').middleware(['auth'])
-  Route.post('/edit/:id', 'UsersController.update').as('update').middleware(['auth'])
-  Route.get('/delete/:id', 'UsersController.destroy').as('destroy').middleware(['auth'])
+  Route.get('/', 'UsersController.index').as('index')
+  Route.get('/profile/:idUser', 'UsersController.profile').as('profile')
+  Route.get('/change-password', 'UsersController.changePassword').as('change.password')
+  Route.post('/change-password/save', 'UsersController.saveChangesPassword').as('save.password')
+  Route.get('/edit/:id', 'UsersController.edit').as('edit')
+  Route.post('/edit/:id', 'UsersController.update').as('update')
+  Route.get('/delete/:id', 'UsersController.destroy').as('destroy')
 })
   .prefix('users')
   .as('users')
+  .middleware(['auth'])
