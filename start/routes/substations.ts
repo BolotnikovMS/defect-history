1eb7c417f565c26b7b3cae4301fbf1aa88c9fa86
BolotnikovMS <bolotnikovms@yandex.ru>
2023-05-14 19:12:05 +0500

    Правки и добавлены маршрут с присоединениями

--- start/routes/substations.ts
+++ start/routes/substations.ts
@@ -1,13 +1,17 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('/', 'SubstationsController.index').as('index').middleware(['auth'])
-  Route.get('/new', 'SubstationsController.create').as('create').middleware(['auth'])
-  Route.post('/new', 'SubstationsController.store').as('store').middleware(['auth'])
+  Route.get('/', 'SubstationsController.index').as('index')
+  Route.get('/new', 'SubstationsController.create').as('create')
+  Route.post('/new', 'SubstationsController.store').as('store')
   Route.get('/show/:id', 'SubstationsController.show').as('show')
-  Route.get('/edit/:id', 'SubstationsController.edit').as('edit').middleware(['auth'])
-  Route.post('/edit/:id', 'SubstationsController.update').as('update').middleware(['auth'])
-  Route.get('/delete/:id', 'SubstationsController.destroy').as('destroy').middleware(['auth'])
+  Route.get('/show-accession/:idSubstation', 'SubstationsController.showAttachments').as(
+    'show.attachments'
+  )
+  Route.get('/edit/:id', 'SubstationsController.edit').as('edit')
+  Route.post('/edit/:id', 'SubstationsController.update').as('update')
+  Route.get('/delete/:id', 'SubstationsController.destroy').as('destroy')
 })
   .prefix('/substations')
   .as('substations')
+  .middleware(['auth'])
