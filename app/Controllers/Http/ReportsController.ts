5597ff2bcbdef80dd8db5602102cd660ef470dcd
BolotnikovMS <bolotnikovms@yandex.ru>
2023-07-16 21:38:21 +0500

    feat: :sparkles: Добавлены метод getDistrictDefects
    Добавлен метод получающий данные о дефектах по выбранному району и фильтру

--- app/Controllers/Http/ReportsController.ts
+++ app/Controllers/Http/ReportsController.ts
@@ -1,7 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Substation from 'App/Models/Substation'
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
 import District from 'App/Models/District'
+import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
 
 export default class ReportsController {
   public async showSubstationDefects({ response, view, session, bouncer }: HttpContextContract) {
@@ -120,6 +120,12 @@ export default class ReportsController {
   }
 
   public async showDistrictDefects({ response, view, session, bouncer }: HttpContextContract) {
+    if (await bouncer.denies('viewReportDistrictDefects')) {
+      session.flash('dangerMessage', 'У вас нет доступа к данной странице!')
+
+      return response.redirect().toPath('/')
+    }
+
     const districts = await District.query()
 
     return view.render('pages/reports/districts_defect/index', {
@@ -129,6 +135,69 @@ export default class ReportsController {
       },
       districts,
     })
+  }
+
+  public async getDistrictDefects({
+    request,
+    response,
+    view,
+    session,
+    bouncer,
+  }: HttpContextContract) {
+    if (await bouncer.denies('viewReportDistrictDefects')) {
+      session.flash('dangerMessage', 'У вас нет доступа к данной странице!')
+
+      return response.redirect().toPath('/')
+    }
+
+    const districts = await District.query()
+    const validationSchema = schema.create({
+      district: schema.number(),
+      filter: schema.string(),
+    })
+    const customMessages: CustomMessages = {
+      required: 'Поле является обязательным.',
+    }
+    const validateData = await request.validate({
+      schema: validationSchema,
+      messages: customMessages,
+    })
+    const district = await District.find(validateData.district)
+    let titleText: string = ''
+
+    if (district) {
+      await district.load((loader) => {
+        loader.load('substations', (substationsQuery) => {
+          substationsQuery.preload('defects', (defectsQuery) => {
+            titleText = 'всех'
+            defectsQuery
+              .if(validateData.filter === 'openDefects', (query) => {
+                titleText = 'открытых'
+                query.whereNull('result')
+              })
+              .if(validateData.filter === 'closeDefects', (query) => {
+                titleText = 'закрытых'
+                query.whereNotNull('result')
+              })
+              .orderBy('term_elimination', 'asc')
+              .preload('defect_type')
+              .preload('accession')
+          })
+        })
+      })
+
+      return view.render('pages/reports/districts_defect/index', {
+        title: `Список ${titleText} дефектов по "${district?.name}"`,
+        messages: {
+          noContent: 'Отчет не сформирован.',
+        },
+        district,
+        districts,
+      })
+    } else {
+      session.flash('dangerMessage', 'Что-то пошло не так!')
+      response.redirect().toRoute('reports.show.district.defects')
+    }
   }
 
   public async index({}: HttpContextContract) {}
