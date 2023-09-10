0cd2091fb9b63e6c97812f8fcbd67727f77c906e
BolotnikovMS <bolotnikovms@yandex.ru>
2023-09-10 18:52:51 +0500

    fix: Убран лишний /

--- start/routes/work_planning.ts
+++ start/routes/work_planning.ts
@@ -2,7 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/defect/:idDefect/new', 'WorkPlanningsController.create').as('create')
-  Route.post('/defect//:idDefect/new', 'WorkPlanningsController.store').as('store')
+  Route.post('/defect/:idDefect/new', 'WorkPlanningsController.store').as('store')
   Route.get('/defect/:idDefect/edit/:id', 'WorkPlanningsController.edit').as('edit')
   Route.post('/defect/:idDefect/edit/:id', 'WorkPlanningsController.update').as('update')
   Route.get('/delete/:id', 'WorkPlanningsController.destroy').as('destroy')
