8a3be12d07e531c0e25cc557c4127d22eb7f0f1a
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-05 15:17:07 +0500

    fix: Fix

--- app/Validators/CloseDefectValidator.ts
+++ app/Validators/CloseDefectValidator.ts
@@ -1,4 +1,5 @@
-import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
+
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class CloseDefectValidator {
@@ -8,15 +9,15 @@ export default class CloseDefectValidator {
     employee: schema.string(),
     description_results: schema.string({}, [
       rules.trim(),
-      rules.escape(),
       rules.minLength(2),
       rules.maxLength(700),
+      rules.escape(),
     ]),
   })
 
   public messages: CustomMessages = {
     required: 'Поле является обязательным.',
     minLength: 'Минимальная длина поля {{ options.minLength }} символа.',
-    maxLength: 'Максимальная длинна поля {{ options.maxLength }} символов',
+    maxLength: 'Максимальная длина поля {{ options.maxLength }} символов',
   }
 }
