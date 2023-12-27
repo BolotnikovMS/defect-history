28453319f860e1a628d0876f19e7e8a3fabe9761
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-27 17:11:51 +0500

    feat: Добавлены поля

--- app/Interfaces/QueryParams.ts
+++ app/Interfaces/QueryParams.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export interface IQueryParams {
-  status?: string
-  department?: number
+  status: string
+  department: number
+  typeDefect: string | number | undefined
   substation: number
   district: number
 }
