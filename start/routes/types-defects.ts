a649c608b4fc44d111fde9f59afa42d98a2bc3b1
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-08 18:19:45 +0500

    fix: Фикс

--- start/routes/types-defects.ts
+++ start/routes/types-defects.ts
@@ -4,6 +4,7 @@ Route.group(() => {
   Route.get('/', 'DefectTypesController.index').as('index').middleware(['auth'])
   Route.get('/new', 'DefectTypesController.create').as('create').middleware(['auth'])
   Route.post('/new', 'DefectTypesController.store').as('store').middleware(['auth'])
+  // Роут больше не поддерживается тк фильтр переработан
   Route.get('/show/:id', 'DefectTypesController.show').as('show')
   Route.get('/edit/:id', 'DefectTypesController.edit').as('edit').middleware(['auth'])
   Route.post('/edit/:id', 'DefectTypesController.update').as('update').middleware(['auth'])
