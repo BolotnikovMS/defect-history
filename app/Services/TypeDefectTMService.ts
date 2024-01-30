bd15d5cc8b723a5891fd73f772eecab1a1dd21e2
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-30 16:23:49 +0500

    feat: Добавлен метод для сохранения

--- app/Services/TypeDefectTMService.ts
+++ app/Services/TypeDefectTMService.ts
@@ -1,5 +1,7 @@
+import { AuthContract } from '@ioc:Adonis/Addons/Auth'
 import { RequestContract } from '@ioc:Adonis/Core/Request'
 import DefectType from 'App/Models/DefectType'
+import TypesDefectValidator from 'App/Validators/TypesDefectValidator'
 
 export default class TypeDefectTMService {
   public static async getTypeDefectTM(req: RequestContract, limit: number = 15) {
@@ -13,5 +15,16 @@ export default class TypeDefectTMService {
     typesDefects.baseUrl('/types-defects')
 
     return typesDefects
+  }
+  public static async store(req: RequestContract, auth: AuthContract): Promise<DefectType> {
+    const validatedData = await req.validate(TypesDefectValidator)
+    const typeDefect = await DefectType.create({
+      id_user_created: auth.user?.id,
+      id_distribution_group: validatedData.group,
+      type_defect: validatedData.type_defect,
+      defect_description: validatedData.defect_description,
+    })
+
+    return typeDefect
   }
 }
