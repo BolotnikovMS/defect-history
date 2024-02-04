5e092257ef6e73fdf72da2cd78d0946aa5b974cd
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-04 19:23:07 +0500

    fix: Добавлен с толбец с ид пользователя, правка названия таблицы

--- database/migrations/1706701853139_defect_classifiers.ts
+++ database/migrations/1706701853139_defect_classifiers.ts
@@ -7,12 +7,19 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table
+        .integer('id_user_created', 10)
+        .notNullable()
+        .unsigned()
+        .index()
+        .references('id')
+        .inTable('users')
+      table
         .integer('id_group_defect', 10)
         .notNullable()
         .unsigned()
         .index()
         .references('id')
-        .inTable('group_defects')
+        .inTable('defect_groups')
       table.text('name').notNullable()
 
       /**
