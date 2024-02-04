d5f3a17bae4df54aee98f643fafad70c1e9ed5d8
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-04 19:27:58 +0500

    feat: Добавлено поле с ид, описана связь

--- app/Models/DefectClassifier.ts
+++ app/Models/DefectClassifier.ts
@@ -1,11 +1,15 @@
 import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 
-import GroupDefect from 'App/Models/GroupDefect'
+import DefectGroup from 'App/Models/DefectGroup'
+import User from 'App/Models/User'
 import { DateTime } from 'luxon'
 
 export default class DefectClassifier extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public id_user_created: number
 
   @column()
   public id_group_defect: number
@@ -19,9 +23,15 @@ export default class DefectClassifier extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
-  @belongsTo(() => GroupDefect, {
+  @belongsTo(() => User, {
+    foreignKey: 'id_user_created',
+    localKey: 'id',
+  })
+  public user_created: BelongsTo<typeof User>
+
+  @belongsTo(() => DefectGroup, {
     foreignKey: 'id_group_defect',
     localKey: 'id',
   })
-  public defect_classifiers: BelongsTo<typeof GroupDefect>
+  public defect_classifiers: BelongsTo<typeof DefectGroup>
 }
