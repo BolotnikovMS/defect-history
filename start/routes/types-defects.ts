55d3f12ea6186b46c2b6932696c7d415c7730f13
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-19 20:05:22 +0500

    refactor: Рефакторинг

--- start/routes/types-defects.ts
+++ start/routes/types-defects.ts
@@ -1,14 +1,15 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('/', 'DefectTypesController.index').as('index').middleware(['auth'])
-  Route.get('/new', 'DefectTypesController.create').as('create').middleware(['auth'])
-  Route.post('/new', 'DefectTypesController.store').as('store').middleware(['auth'])
+  Route.get('/', 'DefectTypesController.index').as('index')
+  Route.get('/new', 'DefectTypesController.create').as('create')
+  Route.post('/new', 'DefectTypesController.store').as('store')
   // Роут больше не поддерживается тк фильтр переработан
   Route.get('/show/:id', 'DefectTypesController.show').as('show')
-  Route.get('/edit/:id', 'DefectTypesController.edit').as('edit').middleware(['auth'])
-  Route.post('/edit/:id', 'DefectTypesController.update').as('update').middleware(['auth'])
-  Route.get('/delete/:id', 'DefectTypesController.destroy').as('destroy').middleware(['auth'])
+  Route.get('/edit/:id', 'DefectTypesController.edit').as('edit')
+  Route.post('/edit/:id', 'DefectTypesController.update').as('update')
+  Route.get('/delete/:id', 'DefectTypesController.destroy').as('destroy')
 })
   .prefix('/types-defects')
   .as('types-defects')
+  .middleware(['auth'])
