ec75612704c0c6043eb2ed0297bd66fdf24f9d24
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-25 21:53:04 +0500

    feat: Сервис с методами district

---
+++ app/Services/DistrictService.ts
@@ -0,0 +1,26 @@
+import { IDistrictParams } from 'App/Interfaces/DistrictParams'
+import District from 'App/Models/District'
+
+export default class DistrictService {
+  public static async getDistricts(params: IDistrictParams): Promise<District[]> {
+    const { req, limit = 15, closedDefects, openedDefects } = params
+    const page = req ? req.input('page', 1) : 1
+    const districts = await District.query()
+      .orderBy('created_at', 'asc')
+      .preload('district_defects', (query) =>
+        query
+          .if(closedDefects, (query) => query.whereNotNull('result'))
+          .if(openedDefects, (query) => query.whereNull('result'))
+      )
+      .preload('district_defects_os', (query) =>
+        query
+          .if(closedDefects, (query) => query.whereNotNull('result'))
+          .if(openedDefects, (query) => query.whereNull('result'))
+      )
+      .paginate(page, limit)
+
+    districts.baseUrl('/districts')
+
+    return districts
+  }
+}
