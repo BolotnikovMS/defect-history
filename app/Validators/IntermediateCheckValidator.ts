b3ed270c0943b7f43efcf1653180e557a5944a21
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-21 18:28:30 +0500

    feat: Добавлено поле типа дефекта

--- app/Validators/IntermediateCheckValidator.ts
+++ app/Validators/IntermediateCheckValidator.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
-import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
+
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class IntermediateCheckValidator {
@@ -9,11 +10,13 @@ export default class IntermediateCheckValidator {
     employee: schema.string(),
     description_results: schema.string({}, [rules.trim(), rules.escape(), rules.minLength(2), rules.maxLength(700)]),
     transferred: schema.number.optional(),
+    type_defect: schema.enum(['tm', 'os', 'rs'] as const),
   })
 
   public messages: CustomMessages = {
     'required': 'Поле является обязательным.',
     'minLength': 'Минимальная длина поля {{ options.minLength }} символа.',
     'maxLength': 'Максимальная длинна поля {{ options.maxLength }} символов',
+    'enum': 'Поле "тип дефекта" должно содержать одно из значений: tm, os, rs ',
   }
 }
