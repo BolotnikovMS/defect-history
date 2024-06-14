2d1b1b80c983f8d5b4890e80b47a5f84a109d47c
BolotnikovMS <bolotnikovms@yandex.ru>
2024-06-14 21:07:36 +0500

    refactor: Убраны маршруты
    Убраны маршруты которые не используются, также добавлены для новых отчетов

--- start/routes/reports.ts
+++ start/routes/reports.ts
@@ -1,22 +1,16 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('/substations-defects', 'ReportsController.showSubstationDefects').as(
-    'show.substation.defects'
-  )
-  Route.get('/substations-defects/get', 'ReportsController.getSubstationDefects').as(
-    'get.substation.defects'
-  )
   Route.get('/district-defects', 'ReportsController.showDistrictDefects').as(
     'show.district.defects'
   )
   Route.get('/district-defects/get', 'ReportsController.getDistrictDefects').as(
     'get.district.defects'
   )
-  Route.get('/all-defects', 'ReportsController.showAllDefects').as('show.all.defects')
-  Route.get('/all-defects/get', 'ReportsController.getAllDefects').as('get.all.defects')
   Route.get('/all-defects-tm', 'ReportsController.showAllDefectsTM').as('show.all.defectsTm')
   Route.get('/all-defects-tm/get', 'ReportsController.getAllDefectsTM').as('get.all.defectsTm')
+  Route.get('/all-defects-os', 'ReportsController.showAllDefectsOS').as('show.all.defectsOs')
+  Route.get('/all-defects-os/get', 'ReportsController.getAllDefectsOS').as('get.all.defectsOs')
 })
   .prefix('/reports')
   .as('reports')
