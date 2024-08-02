80ff018460412da0e0c0c0efa15bbe431fcf563f
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-02 16:39:53 +0500

    feat: Добавлено поле type

---
+++ database/migrations/1721995103662_substation_add_types.ts
@@ -0,0 +1,15 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'substations'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('type', 10).notNullable().defaultTo('ps')
+    })
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {})
+  }
+}
