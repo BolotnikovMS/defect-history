1f23f3d4e9434c09a358b7dcdcafafb03fa48437
Mikhail <bolotnikovms@yandex.ru>
2025-06-18 16:09:49 +0500

    refactor: Поправлен отчет

--- app/Services/ReportService.ts
+++ app/Services/ReportService.ts
@@ -5,7 +5,7 @@ import DefectTMService from './DefectTMService'
 export default class ReportService {
   static #applyStylesRowTitle(worksheet: ExcelJS.Worksheet): void {
     worksheet.getRow(1).eachCell((cell: Cell) => {
-      cell.alignment = { vertical: 'middle', horizontal: 'center' }
+      cell.alignment = { vertical: 'middle', horizontal: 'center', wrapText: true }
       cell.font = { bold: true, size: 15 }
       cell.border = {
         top: { style: 'medium' },
@@ -50,38 +50,28 @@ export default class ReportService {
     const worksheet = workbook.addWorksheet('Sheet1')
 
     worksheet.columns = [
-      { header: 'Дата добавления', key: 'createdAt', width: 20 },
-      { header: 'Объект', key: 'substation', width: 26 },
+      { header: 'Дата добавления', key: 'createdAt', width: 16 },
       { header: 'Тип', key: 'typeDefect', width: 15 },
+      { header: 'Объект', key: 'substation', width: 26 },
       { header: 'Присоединение', key: 'accession', width: 30 },
       { header: 'Описание дефекта', key: 'description', width: 45 },
-      { header: 'Срок устранения', key: 'termElimination', width: 20 },
-      { header: 'Дата устранения', key: 'eliminationDate', width: 20 },
-      { header: 'Результат', key: 'result', width: 45 },
-      { header: 'Промежуточные результаты', key: 'intermediateChecks', width: 45 },
+      { header: 'Срок устранения', key: 'termElimination', width: 16 },
       { header: 'Подготовительные работы', key: 'workPlanning', width: 45 },
+      { header: 'Промежуточные результаты', key: 'intermediateChecks', width: 45 },
+      { header: 'Дата устранения', key: 'eliminationDate', width: 16 },
+      { header: 'Результат', key: 'result', width: 45 },
     ]
 
     this.#applyStylesRowTitle(worksheet)
 
     defects.forEach((defect) => {
       worksheet.addRow({
-        createdAt: defect.created_at.toFormat('dd.MM.yyyy HH:mm'),
-        substation: defect.substation.name,
+        createdAt: defect.created_at.toFormat('dd.MM.yyyy'),
         typeDefect: defect.defect_type.type_defect,
+        substation: defect.substation.name,
         accession: defect.accession.name,
         description: defect.description_defect,
-        termElimination: defect.term_elimination.toFormat('dd.MM.yyyy HH:mm'),
-        eliminationDate: defect.elimination_date?.toFormat('dd.MM.yyyy HH:mm'),
-        result: defect.result,
-        intermediateChecks: defect.intermediate_checks
-          .map(
-            (check, i) =>
-              `${i + 1}. ${check.description_results}. ${check.name_inspector.shortUserName} (${
-                check.name_inspector.department.name
-              })`
-          )
-          .join(',\n'),
+        termElimination: defect.term_elimination.toFormat('dd.MM.yyyy'),
         workPlanning: defect.work_planning
           .map(
             (work, i) =>
@@ -90,6 +80,16 @@ export default class ReportService {
               })`
           )
           .join(',\n'),
+        intermediateChecks: defect.intermediate_checks
+          .map(
+            (check, i) =>
+              `${i + 1}. ${check.description_results}. ${check.name_inspector.shortUserName} (${
+                check.name_inspector.department.name
+              })`
+          )
+          .join(',\n'),
+        eliminationDate: defect.elimination_date?.toFormat('dd.MM.yyyy'),
+        result: defect.result,
       })
     })
 
