63deb8b955988bf071fadab5bf5da9cb1abd904c
Mikhail <bolotnikovms@yandex.ru>
2025-07-15 21:01:24 +0500

    refactor: Пересмотрен интерфейс

--- app/Interfaces/DistrictParams.ts
+++ app/Interfaces/DistrictParams.ts
@@ -1,8 +1,9 @@
 import { RequestContract } from '@ioc:Adonis/Core/Request'
-import { IDefectParams } from './DefectParams'
+import { TStatus } from 'App/Types'
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export interface IDistrictParams extends IDefectParams {
+export interface IDistrictParams {
+  status?: TStatus
   req?: RequestContract
   limit?: number
 }
