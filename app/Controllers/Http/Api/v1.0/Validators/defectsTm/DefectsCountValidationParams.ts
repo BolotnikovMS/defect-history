dde10ba7614a90887cb838ee450e743a099011b4
Mikhail <bolotnikovms@yandex.ru>
2025-07-15 20:29:02 +0500

    feat: Схема валидации параметров запросов

---
+++ app/Controllers/Http/Api/v1.0/Validators/defectsTm/DefectsCountValidationParams.ts
@@ -0,0 +1,17 @@
+import { schema, validator } from '@ioc:Adonis/Core/Validator'
+import { idSubstation, status } from 'App/Validators/fields'
+
+const validatedParamsSchema = schema.create({
+  idSubstation: idSubstation,
+  status: status,
+})
+
+export const defectsCountValidationParams = {
+  schema: validatedParamsSchema,
+  messages: {
+    exists: 'Не удалось найти объект по указанному идентификатору!',
+    number: 'Некорректный формат числа. Параметр должен содержать числовое значение!',
+    enum: 'Допустимые значения параметра: "{{ options.choices }}".',
+  },
+  reporter: validator.reporters.jsonapi,
+}
