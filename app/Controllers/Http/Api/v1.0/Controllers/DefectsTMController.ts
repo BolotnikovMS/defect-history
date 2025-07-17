cb75460a0ce45c44f19dc163dd92ad489f6abcdc
Mikhail <bolotnikovms@yandex.ru>
2025-07-17 20:46:24 +0500

    fix: Удален код

--- app/Controllers/Http/Api/v1.0/Controllers/DefectsTMController.ts
+++ app/Controllers/Http/Api/v1.0/Controllers/DefectsTMController.ts
@@ -3,43 +3,7 @@ import DefectTMService from 'App/Services/DefectTMService'
 import { defectsCountValidationParams } from '../Validators/defectsTm/DefectsCountValidationParams'
 
 export default class DefectsTMController {
-  public async index({ response }: HttpContextContract) {
-    // try {
-    //   const defects = await Defect.query()
-    //     .preload('substation')
-    //     .preload('defect_type')
-    //     .preload('intermediate_checks', (intermediateChecksQuery) => {
-    //       intermediateChecksQuery.preload('name_inspector')
-    //     })
-    //     .preload('name_eliminated')
-    //   const defectsJSON = defects.map((defect) => {
-    //     return defect.serialize({
-    //       relations: {
-    //         substation: {
-    //           fields: ['name'],
-    //         },
-    //         defect_type: {
-    //           fields: ['type_defect'],
-    //         },
-    //         intermediate_checks: {
-    //           fields: ['id', 'check_date', 'test_result'],
-    //           relations: {
-    //             name_inspector: {
-    //               fields: ['fullName', 'position'],
-    //             },
-    //           },
-    //         },
-    //         name_eliminated: {
-    //           fields: ['fullName', 'position'],
-    //         },
-    //       },
-    //     })
-    //   })
-    //   return response.status(200).json(defectsJSON)
-    // } catch (e) {
-    //   return response.badRequest(e.messages)
-    // }
-  }
+  public async index({}: HttpContextContract) {}
 
   public async getCountDefectsByIdSubstation({ request, response, params }: HttpContextContract) {
     const validatedParams = await request.validate({
